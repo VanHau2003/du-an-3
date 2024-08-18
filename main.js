27ecd63e103aa6864a9324dd8008a4cd309f24ce
Anh Hậu <levanhau2019cm@gmail.com>
2024-08-18 13:46:55 +0700

    1st commit

---
+++ main.js
@@ -0,0 +1,24 @@
+alert("Xin Chào các bạn !!");
+// prompt("Mời bạn nhập tên của bạn !!");
+
+//Khai báo biến
+var myName;
+let YourName;
+
+//Khởi tạo biến
+var diemtoan = 10;
+let firstname = "Julia";
+const Doiso = 100;
+//Xuat gia tri cua bien
+
+console.log(diemtoan);
+console.log("Điểm toán của bạn là : " + diemtoan + "Tên của bạn là : "+ firstname );
+
+//Thay đổi giá trị của biến
+diemtoan = 5;
+
+console.log("Điểm toán của bạn là : " + diemtoan);
+firstname = "David";
+console.log(firstname);
+
+//hằng số const không thể thay đổi !!
